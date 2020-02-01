11b0370915ffba86c7ed066048319d9a90da0755
AyantuT <ayantut2022@gmail.com>
2020-02-01 14:28:54 -0800

    Update Robot.cpp

---
+++ Cortex/src/main/cpp/Robot.cpp
@@ -0,0 +1,129 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Robot.h"
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <ctre/Phoenix.h>
+#include <frc/Timer.h>
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/Joystick.h>
+
+//Declarations
+TalonSRX srx = {0};
+
+//Motors
+//Left Side
+TalonFX LeftMotorOne();
+TalonFX LeftMotorTwo();
+TalonFX LeftMotorThree();
+//Right Side
+TalonFX RightMotorOne();
+TalonFX RightMotorTwo();
+TalonFX RightMotorThree();
+
+//Joysticks
+frc::Joystick JoyAccel1{0}, Xbox{1}, RaceWheel{2};
+
+double JoyY = -JoyAccel1.GetY();
+double WheelX = RaceWheel.GetX();
+//Functions
+
+void LeftMotorsSpeed(double speed){
+  LeftMotorOne.Set(ControlMode::PercentOutput, speed);
+  LeftMotorTwo.Set(ControlMode::PercentOutput, speed);
+  LeftMotorThree.Set(ControlMode::PercentOutput, speed);
+}
+void RightMotorsSpeed (double speed) {
+  RightMotorOne.Set(ControlMode::PercentOutput, speed);
+  RightMotorTwo.Set(ControlMode::PercentOutput, speed);
+  RightMotorThree.Set(ControlMode::PercentOutput, speed);
+}
+
+void Robot::RobotInit() {
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  srx.Set(ControlMode::PercentOutput, 0);
+
+
+}
+
+/**
+ * This function is called every robot packet, no matter the mode. Use
+ * this for items like diagnostics that you want ran during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic() {}
+
+/**
+ * This autonomous (along with the chooser code above) shows how to select
+ * between different autonomous modes using the dashboard. The sendable chooser
+ * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+ * remove all of the chooser code and uncomment the GetString line to get the
+ * auto name from the text box below the Gyro.
+ *
+ * You can add additional auto modes by adding additional comparisons to the
+ * if-else structure below with additional strings. If using the SendableChooser
+ * make sure to add them to the chooser code above as well.
+ */
+void Robot::AutonomousInit() {
+  m_autoSelected = m_chooser.GetSelected();
+  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
+  //     kAutoNameDefault);
+  std::cout << "Auto selected: " << m_autoSelected << std::endl;
+
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::AutonomousPeriodic() {
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::TeleopInit() {}
+
+void Robot::TeleopPeriodic() {
+  //Joysticks
+  JoyY = -JoyAccel1.GetY() * 0.1;
+  WheelX = RaceWheel.GetX() * 0.1;
+
+  //Drive Code
+  //Button 5 on the wheel activates point turning
+  if (RaceWheel.GetRawButton(5)) {
+    LeftMotorsSpeed(WheelX);
+    RightMotorsSpeed(-WheelX);
+  } 
+   //Regular Turning
+  else if((WheelX < -0.01 || WheelX > 0.01) && (JoyY > 0.06 || JoyY < -0.06)){
+    LeftMotorsSpeed(JoyY + WheelX);
+    RightMotorsSpeed(JoyY - WheelX);
+  }
+  //Code for driving straight  
+  else if (JoyY > 0.1|| JoyY < -0.1 ) {
+    LeftMotorsSpeed(JoyY);                 
+    RightMotorsSpeed(JoyY);
+  }
+}
+
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
