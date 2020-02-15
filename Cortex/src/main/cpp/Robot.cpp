a9c943b46b36e4398affbe3e6a369245c87cebfd
LoganW5458 <lw807@wjusd.org>
2020-02-15 13:55:53 -0800

    Update Robot.cpp

--- Cortex/src/main/cpp/Robot.cpp
+++ Cortex/src/main/cpp/Robot.cpp
@@ -37,6 +37,9 @@ TalonFX RightMotorThree{0};
 //PDP
 frc::PowerDistributionPanel pdp{0};
 
+//Color Sensor
+TalonSRX colorMotor{5};
+
 //Gyro
 frc::ADXRS450_Gyro *gyro;
 
@@ -205,6 +208,11 @@ void rotationalAcceleration() {
 
   rotationalAcceleration
   LeftMotorsSpeed(motorAccelerationSpeed);
+
+  Steps:
+  1. correct driving during teleop
+  2. correct turn function so it goes until sum angle = 180
+  3. make turn function so it does sqrt function above
   */
 } 
 
@@ -374,13 +382,13 @@ void Robot::TeleopPeriodic() {
   } 
    //Regular Turning
   else if((WheelX < -0.05 || WheelX > 0.05) && (JoyY > 0.05 || JoyY < -0.05)){
-    LeftMotorsSpeed(accelerationSpeed + fabs(accelerationSpeed) * (turnFact * WheelX));
-    RightMotorsSpeed(accelerationSpeed - fabs(accelerationSpeed) * (turnFact * WheelX));
+    LeftMotorsSpeed(accelerationSpeed + (fabs(accelerationSpeed) * (turnFact * WheelX)));
+    RightMotorsSpeed(accelerationSpeed - (fabs(accelerationSpeed) * (turnFact * WheelX)));
   }
   //Code for driving straight  
   else if (JoyY > 0.05 || JoyY < -0.05){
-    LeftMotorsSpeed(accelerationSpeed - correctionAngle);                 
-    RightMotorsSpeed(accelerationSpeed - correctionAngle);
+    LeftMotorsSpeed(accelerationSpeed /*- correctionAngle*/);                 
+    RightMotorsSpeed(accelerationSpeed /*- correctionAngle*/);
   } 
   //Code for if nothing is pressed
   else {
@@ -397,9 +405,10 @@ void Robot::TeleopPeriodic() {
   //Putting values into Shuffleboard
   frc::SmartDashboard::PutNumber("Gyro Angle", gyro->GetAngle());
   frc::SmartDashboard::PutNumber("Gyro Rate", gyro->GetRate());
-  //Get encoder values from falcons (built in encoders)
+  //Get encoder values from falcons (built in encoders) and other motors
   frc::SmartDashboard::PutNumber("RightEncoderOne", RightMotorOne.GetSelectedSensorPosition());
   frc::SmartDashboard::PutNumber("LeftEncoderOne", LeftMotorOne.GetSelectedSensorPosition());
+  frc::SmartDashboard::PutNumber("Color Encoder", colorMotor.GetSelectedSensorPosition());
   //Get the current speed of each side
   frc::SmartDashboard::PutNumber("RightMotorSpeed", RightMotorOne.GetMotorOutputPercent());
   frc::SmartDashboard::PutNumber("LeftMotorsSpeed", LeftMotorOne.GetMotorOutputPercent());
